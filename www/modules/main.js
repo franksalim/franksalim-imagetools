a4272b91cf841ae93f24db9a24acc17438535588
meat popsickle <anon@moose>
2022-09-10 01:41:00 -0700

    Ignore keypresses when the user is typing in an input

--- www/modules/main.js
+++ www/modules/main.js
@@ -7,6 +7,11 @@ import {ToolPicker} from "/modules/panels/toolpicker.js";
 
 // navigate history using the j and k keys
 document.addEventListener('keydown', e => {
+  // but don't do anything if the user is typing in a text field
+  const composedDirectTarget = e.composedPath()[0];
+  if (composedDirectTarget.tagName == "INPUT" || composedDirectTarget.tagName == "TEXTAREA") {
+    return;
+  }
   if (e.key === 'j') {
     document.getElementById("historyList").selectNext();
   } else if (e.key === 'k') {
