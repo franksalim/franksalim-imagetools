e61c7f8ad6d64a69c1680af76461767c7da68995
Frank Salim <frank.salim@gmail.com>
2022-10-09 16:22:19 -0700

    about panel

---
+++ www/modules/panels/about.js
@@ -0,0 +1,25 @@
+export class About extends HTMLElement {
+  constructor() {
+    super();
+    let shadow = this.attachShadow({mode: 'open'});
+    shadow.innerHTML = `
+      <link rel=stylesheet href=/css/panel.css>
+      <style>
+        h2, a {
+          display: block;
+          margin: 8px 0px;
+        }
+      </style>
+
+      <h2>franksalim-imagetools</h2>
+      <a href="https://github.com/franksalim/franksalim-imagetools">This project on GitHub</a>
+      
+      <h2>Open Source Dependencies</h2>
+      <a href="https://github.com/huggingface/diffusers/">Diffusers</a>
+      <a href="https://github.com/CompVis/stable-diffusion">Stable Diffusion</a>
+      <a href="https://github.com/pallets/flask">Flask</a>
+    `;
+  }
+}
+
+window.customElements.define('fs-about', About);
